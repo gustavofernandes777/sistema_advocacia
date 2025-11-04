0bb4316056523093881eb8b72ca41ccf14564289
GustavoFernandesTE <gustavorodrigues_fernandes@hotmail.com>
2025-11-04 13:16:36 -0300

    Update postToSlack.js

--- js/postToSlack.js
+++ js/postToSlack.js
@@ -79,7 +79,7 @@ export async function postMessageToSlack(mensagem) {
     try {
         const response = await apiFetch(`${apiBaseUrl}/api/slack`, {
             method: "POST",
-            body: JSON.stringify({ message: mensagem }),
+            body: JSON.stringify({mensagem}),
         });
 
         Swal.fire({
