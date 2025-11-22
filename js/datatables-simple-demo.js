eb5f9c404c471259eedbe6195dfe34a239ea051b
GustavoFernandesTE <gustavorodrigues_fernandes@hotmail.com>
2025-11-22 17:02:05 -0300

    update

--- js/datatables-simple-demo.js
+++ js/datatables-simple-demo.js
@@ -1,6 +1,13 @@
 window.initDataTable = function () {
     const table = document.getElementById('datatablesSimple');
+
     if (table) {
-        new simpleDatatables.DataTable(table);
+        const dt = new simpleDatatables.DataTable(table);
+
+        // Depois que o DataTable reconstruir o DOM
+        setTimeout(() => {
+            const newTbody = table.querySelector('tbody');
+            if (newTbody) newTbody.id = "records-body";
+        }, 0);
     }
 };
