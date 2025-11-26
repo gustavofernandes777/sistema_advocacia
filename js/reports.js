a643669fe2e05f859342b2a62f9b011b66ba0b4b
GustavoFernandesTE <gustavorodrigues_fernandes@hotmail.com>
2025-11-26 18:53:01 -0300

    update

--- js/reports.js
+++ js/reports.js
@@ -621,7 +621,7 @@ function showRecordModal(record) {
         <div class="modal fade" id="recordModal" tabindex="-1">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
-                    <div class="modal-header">
+                    <div class="modal-header header-text">
                         <h5 class="modal-title">Detalhes do Registro: ${record.record_id}</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                     </div>
